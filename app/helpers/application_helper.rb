1512b9bbf71f64e4d903d0126515d5eca731dc17
Sebastian Menge <s.menge@gmx.net>
2011-09-28 22:34:25 +0200

    finished the startpage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,18 @@
 module ApplicationHelper
+
+  def title(page_title)
+    content_for(:title) {page_title}
+  end  
+  
   def resource_name
-      :user
-    end
+    :user
+  end
 
-    def resource
-      @resource ||= User.new
-    end
+  def resource
+    @resource ||= User.new
+  end
 
-    def devise_mapping
-      @devise_mapping ||= Devise.mappings[:user]
-    end
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
